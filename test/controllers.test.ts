628a79a9c45c21bb12c85463ace649ab330e4f2d
Johnny <huging@gmail.com>
2022-09-01 23:48:32 +1200

    fix env for test

--- test/controllers.test.ts
+++ test/controllers.test.ts
@@ -1,10 +1,10 @@
 import { Model } from 'objection';
 import { Knex, knex } from 'knex';
+// import dotenv from 'dotenv';
 import * as dotenv from 'dotenv';
 import { startNewDraw } from '../src/controller/drawController';
 
-
-dotenv.config({ path: __dirname + '/.env' });
+dotenv.config();
 
 const testDbConn: Knex.Config = {
   client: 'postgres',
@@ -22,16 +22,17 @@ const testConn = knex(testDbConn);
 describe('lottery unit test', () => {
   beforeAll(async () => {
     await testConn.migrate.latest();
-    await testConn.seed.run();
     Model.knex(testConn);
   });
   test('dummy test', async () => {
-    await startNewDraw();
+    const drawCreated = await startNewDraw();
+    console.log(drawCreated);
+    
     expect(1).toEqual(1);
   });
   afterAll(async () => {
     console.log(' 12345 ');
-    //await testConn.migrate.rollback();
+    await testConn.migrate.rollback();
     await testConn.destroy();
     console.log(' 67890 ');
   });
